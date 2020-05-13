1963ddc033a9c0ed3305000a60aeb29a4cceb745
ponpoko1968 <ponpoko1968@gmail.com>
2020-05-13 17:07:06 +0900

    create new task, then start it.

--- PaymoTasks.py
+++ PaymoTasks.py
@@ -15,32 +15,81 @@ class PaymoTasks:
     homePath = os.path.expanduser('~')
     filePath = homePath + "/.paymoapi.secret.json"
     project_dict = dict()
+
+    def get_user_id(self):
+        return os.getenv('paymo_user_id')
+
     def api_key(self):
-        try:
-            with open(self.filePath) as secret:
-                data = json.load(secret)
-                return  (data["secret"], None) if 'secret' in data else (None, "error: missing key 'secret'")
-        except FileNotFoundError:
+        key = os.getenv('paymo_api_key')
+        if key:
+            return (key, None)
+        else:
             return (None, 'error: ~/.paymoapi.secret.json not found.\ncreate it.')
-
-
-    def getProjects(self):
+        # try:
+        #     with open(self.filePath) as secret:
+        #         data = json.load(secret)
+        #         return  (data["secret"], None) if 'secret' in data else (None, "error: missing key 'secret'")
+        # except FileNotFoundError:
+        #     return (None, 'error: ~/.paymoapi.secret.json not found.\ncreate it.')
+    def getClients(self):
         api_key ,_ = self.api_key()
         if not api_key:
             return []
-        url = BASE_URL + '/projects'
+        url = BASE_URL + '/clients'
         basic_user_and_pasword = base64.b64encode('{}:{}'.format(api_key, 'X').encode('utf-8'))
         request = urllib.request.Request(url,
                                          headers={"Authorization": "Basic " + basic_user_and_pasword.decode('utf-8')})
         with urllib.request.urlopen(request) as f:
             try:
-                projects = json.loads(f.read())['projects']
-                for proj in projects:
-                    self.project_dict[proj['id']] = proj['name']
+                return json.loads(f.read())['clients']
             except ValueError:
                 return []
             else:
                 return []
+
+    def getUsers(self):
+        api_key ,_ = self.api_key()
+        if not api_key:
+            return []
+        url = BASE_URL + '/users'
+        basic_user_and_pasword = base64.b64encode('{}:{}'.format(api_key, 'X').encode('utf-8'))
+        request = urllib.request.Request(url,
+                                         headers={"Authorization": "Basic " + basic_user_and_pasword.decode('utf-8')})
+        with urllib.request.urlopen(request) as f:
+            try:
+                return json.loads(f.read())
+            except ValueError:
+                return []
+            else:
+                return []
+
+    def getProjects(self, client_id=None):
+        api_key ,_ = self.api_key()
+        if not api_key:
+            return []
+        url = BASE_URL + '/projects?where=client_id={}'.format(client_id) if client_id else BASE_URL + '/projects'
+        print(url, file=sys.stderr)
+        basic_user_and_pasword = base64.b64encode('{}:{}'.format(api_key, 'X').encode('utf-8'))
+        request = urllib.request.Request(url,
+                                         headers={"Authorization": "Basic " + basic_user_and_pasword.decode('utf-8')})
+        with urllib.request.urlopen(request) as f:
+            try:
+                project_dict = {}
+                projects = json.loads(f.read())['projects']
+                for proj in projects:
+                    #print(proj.keys())
+                    project_dict[proj['id']] = proj
+                return project_dict
+            except ValueError:
+                return []
+            else:
+                return []
+
+    def outputClients(self, client_list):
+        func = lambda client: {'title': client['name'],
+                              'match': client['name'],
+                              'arg': client['id']}
+        print(json.dumps( {'items': [func(client) for client in client_list]} ))
 
     def getTasks(self):
         api_key ,_ = self.api_key()
@@ -58,11 +107,45 @@ class PaymoTasks:
             else:
                 return []
 
+    def createTask(self, project_id, task_name, start=True):
+        api_key ,_ = self.api_key()
+        if not api_key:
+            return []
+        url = BASE_URL + '/tasks'
+        basic_user_and_pasword = base64.b64encode('{}:{}'.format(api_key, 'X').encode('utf-8'))
+        json_data = json.dumps({'project_id': project_id,
+            'name': task_name }).encode('utf-8')
+        print(json_data, file=sys.stderr)
+        request = urllib.request.Request(url,
+                                         data=json_data, 
+                                         method='POST',
+                                         headers={
+                                             "Content-Type" : "application/json",
+                                             "Authorization": "Basic " + basic_user_and_pasword.decode('utf-8')})
+        try:
+            with urllib.request.urlopen(request) as response:
+                data = response.read().decode("utf-8")
+                resp = json.loads(data)
+                print(resp, file=sys.stderr)
+                if start and 'tasks' in resp and len(resp['tasks']) > 0:
+                    self.startTask(int(resp['tasks'][0]['id']))
+        except urllib.error.HTTPError as identifier:
+            print(identifier)
+            print(identifier.read())
+
+    def outputProjects(self, client_id=None):
+        projects = self.getProjects(client_id)
+        func = lambda k: {'title': projects[k]['name'],
+                              'subtitle' : '',
+                              'match': projects[k]['name'],
+                              'arg':k}
+        print(json.dumps( {'items': [func(key) for key in projects.keys()]} ))
+
     def outputTasks(self):
         self.getProjects()
         func = lambda i: {'title': i['name'],
-                          'subtitle' : self.project_dict[i['project_id']],
-                          'match': '{} {} {}'.format(i['name'], i['code'], self.project_dict[i['project_id']]),
+                         # 'subtitle' : self.project_dict[i['project_id']]['name'],
+                          'match': '{} {} {}'.format(i['name'], i['code'], self.project_dict[i['project_id']]['name']),
                           'arg':i['id']}
         print(json.dumps({'items':[func(task) for task in self.getTasks()]}))
 
@@ -113,9 +196,15 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('--list', action='store_true')
     parser.add_argument('--start', nargs=1)
+    parser.add_argument('--client-id', nargs=1)
+    parser.add_argument('--project-list', action='store_true')
+    parser.add_argument('--client-list', action='store_true')
+    parser.add_argument('--task-name', nargs=1)
+    parser.add_argument('--project-id', nargs=1)
     args = parser.parse_args()
     task = PaymoTasks()
     (_, error) = task.api_key()
+
     if error:
         print( json.dumps({"items": [{'title': 'ERROR',
                                       'subtitle' : error,
@@ -127,4 +216,14 @@ if __name__ == '__main__':
         print(int(args.start[0]), file=sys.stderr)
         PaymoTasks().startTask(int(args.start[0]))
         sys.exit(0)
-    PaymoTasks().outputTasks()
+    elif args.project_list:
+        if args.client_id and len(args.client_id)==1:
+            PaymoTasks().outputProjects(client_id=args.client_id[0])
+        else:
+            PaymoTasks().outputProjects()
+    elif args.client_list:
+        PaymoTasks().outputClients(PaymoTasks().getClients())
+    elif args.task_name and args.project_id:
+        PaymoTasks().createTask(args.project_id[0], args.task_name[0] )
+    else:
+        PaymoTasks().outputTasks()
